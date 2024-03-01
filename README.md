67311055e0edda2ef8738fa328583750cc6ffb95
Gökhan Uymur <gkhanuymur@hotmail.com>
2024-03-01 22:01:21 +0300

    first pushed

--- README.md
+++
@@ -1,1 +0,0 @@
-# Swift-Projects-With-Swift-UI
---
+++ SwiftUI Weather App/SwiftUI Weather App/SwiftUI_Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Weather_AppApp.swift
+//  SwiftUI Weather App
+//
+//  Created by Gökhan Uymur on 1.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
